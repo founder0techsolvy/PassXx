57b93532ec047a1ac5ce623d924afd65a4555a14
Avinash Mishra <founder.techsolvy@gmail.com>
2024-11-25 07:09:08 +0530

    Create login.js

---
+++ pages/login.js
@@ -0,0 +1,12 @@
+
+    // https://bytewebster.com/
+    const signupBtn = document.getElementById("signup-btn");
+    const signinBtn = document.getElementById("signin-btn");
+    const mainContainer = document.querySelector(".container");
+    
+    signupBtn.addEventListener("click", () => {
+      mainContainer.classList.toggle("change");
+    });
+    signinBtn.addEventListener("click", () => {
+      mainContainer.classList.toggle("change");
+    });
